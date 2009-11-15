a6dca9264e30281e9438baffbf89351ad0272162
Tamas Nagy <tamas.nagy8313+github@gmail.com>
2009-11-15 13:45:14 +0000

    Announce to tracker first version
    Announce does not work perfectly right now.
    The tracker returns an error, but it is getting there.

--- src/torrent_manager.erl
+++ src/torrent_manager.erl
@@ -34,7 +34,30 @@
 
 -include("torrenterl_types.hrl").
 
--record(torrent, {announce, info_hash}).
+-record(torrent, {
+      announce
+    , infohash
+    , piecelength
+    , hashlist
+    , lastpiece
+    , nbofpieces
+    , private= 0
+    , type
+    , name
+    , length
+    , peerid= gen_peer_id()
+    , trackerid  
+    }).
+
+-record(torrent_stats,{
+      infohash
+    , uploaded= 0
+    , downloaded= 0 
+    , left
+    }).
+
+%%TODO: Options: Download directory, listening port,
+
 %% @spec (Path) -> Data
 %%   Path = string()
 %%   Data = binary | integer() | list()
@@ -44,15 +67,71 @@
 -spec download(string()) -> ok.
 download(File) ->
     {Data, _Rest} = bencode:decode_file(File),
-    verify_torrent(Data, #torrent{}).
+    Torrent = lists:foldl(fun verify_torrent/2, #torrent{}, Data),
+    TorrentStats = #torrent_stats{infohash= Torrent#torrent.infohash,
+     left= Torrent#torrent.length},
+    send_announce(Torrent, TorrentStats, [{<<"event">>, <<"started">>},
+     {<<"compact">>, <<"1">>}, %%NOTE: Only support compact response for now.
+     {<<"numwant">>, <<"50">>}]). %%NOTE: First ask for 50 this might change
 
 -spec verify_torrent(data(), record()) -> record().
-verify_torrent([{<<"announce", Url>>} | Rest], Torrent) ->
-    verify_torrent(Rest, Torrent#torrent{announce= Url});
-verify_torrent([{<<"info">>, FileInfo} | Rest], Torrent) ->
-    verify_torrent_info(FileInfo, Torrent). %%TODO: info_hash
+verify_torrent({<<"announce">>, Url}, Torrent) ->
+    Torrent#torrent{announce= binary_to_list(Url)};
+verify_torrent({<<"info">>, FileInfo}, Torrent) ->
+    InfoHash = crypto:sha(bencode:encode(FileInfo)),
+    lists:foldl(fun verify_torrent_info/2, 
+     Torrent#torrent{infohash= InfoHash}, FileInfo);
+verify_torrent(_Else, Torrent) ->
+    Torrent.
 
-%%TODO: analyse one or more files content info
 -spec verify_torrent_info(data(), record()) -> record().
-verify_torrent_info(_, Torrent) -> Torrent.
+verify_torrent_info({<<"piece length">>, PLength}, 
+ Torrent = #torrent{type= single, length= Length}) ->
+    Torrent#torrent{piecelength= PLength, lastpiece= Length rem PLength};
+verify_torrent_info({<<"pieces">>, Pieces}, Torrent) -> 
+    HashList = to_hash_list(Pieces, []),
+    Torrent#torrent{hashlist= HashList, nbofpieces= length(HashList)};
+verify_torrent_info({<<"private">>, Num}, Torrent) -> 
+    Torrent#torrent{private= Num};
+verify_torrent_info({<<"name">>, Name}, Torrent) -> 
+    Torrent#torrent{name= binary_to_list(Name)};
+verify_torrent_info({<<"length">>, Length}, Torrent) -> 
+    Torrent#torrent{type= single, length= Length};
+verify_torrent_info({<<"files">>, _Files}, Torrent) ->
+    Torrent#torrent{type= multi},
+    exit(multi_file_not_supported); %%TODO: Add multi file support 
+verify_torrent_info(_Else, Torrent) -> 
+    Torrent.
 
+-spec to_hash_list(binary(), list())-> [binary()].
+to_hash_list(<<>>, Acc) ->
+    lists:reverse(Acc);
+to_hash_list(<<Hash:20/binary, Rest/binary>>, Acc) ->
+    to_hash_list(Rest, [Hash, Acc]). 
+
+-spec send_announce(record(), record(), list()) -> tuple().
+send_announce(Torrent = #torrent{announce= Announce}, TorrentStats, Params) ->
+    TorrentParams = to_announce_params(Torrent),
+    TorrentStatParams = to_announce_params(TorrentStats),
+    ParamList  = 
+     create_get_param_list(TorrentParams ++ TorrentStatParams ++ Params),
+    lhttpc:request(lists:flatten(Announce ++ ParamList), "GET", [], 5000).
+    
+-spec to_announce_params(record()) -> list(tuple()).
+to_announce_params(#torrent{infohash= InfoHash, peerid= PeerId}) ->
+    [{<<"info_hash">>, list_to_binary(edoc_lib:escape_uri(
+     binary_to_list(InfoHash)))},
+     {<<"peer_id">>, PeerId}];
+to_announce_params(#torrent_stats{
+ uploaded= Up, downloaded= Down, left= Left}) ->
+    [{<<"uploaded">>, list_to_binary(integer_to_list(Up))},
+     {<<"downloaded">>, list_to_binary(integer_to_list(Down))},
+     {<<"left">>, list_to_binary(integer_to_list(Left))}].
+     
+create_get_param_list(List) ->
+    "?" ++ string:join([ [binary_to_list(Key), "=", binary_to_list(Value)]  || 
+        {Key, Value} <- List], "&").
+
+gen_peer_id() ->
+    list_to_binary([<<"-ER0001-">>, 
+     list_to_binary(io_lib:format("~.12.0w",[random:uniform(999999999999)]))]).
